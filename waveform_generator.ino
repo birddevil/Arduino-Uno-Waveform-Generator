d34cc294275a8833372113612efdb7848ecc451d
Ovidiu Panait <ovidiuspn@gmail.com>
2017-03-05 00:35:52 +0200

    Add sine and ramp waveforms

---
+++ waveform_generator.ino
@@ -0,0 +1,67 @@
+/*
+ *  Arduino Uno waveform generator using R-2R resistor ladder as DAC
+ *  ~50KHz max frequency
+ */
+
+#define LT_LEN ((uint16_t)256) //Lookup table length
+#define DEFAULT_FREQ 30000.0 // default waveform frequency
+#define F_CLK  331122.51496207058823529411764705 //aproximate value for the frequency the loop runs at
+
+#define DAC_BITS 6
+#define ACC_LEN 16
+
+#define SINE 0
+#define RAMP 1
+
+uint8_t samples[LT_LEN] ={0}; /* Lookup table */
+uint8_t max_dac_amplitude = ((uint8_t)1 << DAC_BITS) - 1; 
+uint32_t prec_bits = ((uint32_t)1 << ACC_LEN);
+uint32_t phase_acc_max = ((uint32_t)1 << ACC_LEN);
+
+/* We will use them in ISR */
+volatile float frequency = DEFAULT_FREQ;
+volatile uint8_t current_waveform = SINE; 
+volatile uint32_t phase_acumulator = 0;
+volatile uint32_t tuning_word = ((frequency * phase_acc_max) / F_CLK) * prec_bits;
+
+void generate_waveform(){
+
+  switch(current_waveform){
+    case SINE:
+      for (int i = 0; i < LT_LEN; ++i) {
+      samples[i] = (max_dac_amplitude * (1 + sin((float)i * 2 * PI / LT_LEN))/2);
+      }
+    break;
+    case RAMP:
+      for (int i = 0; i < LT_LEN; ++i) {
+        samples[i] = i*((float)max_dac_amplitude / LT_LEN);
+      }
+      break;
+  }
+  
+}
+
+void setup() {
+
+  /* Configure portB as output */
+  DDRB = 0xFF;
+  generate_waveform();
+}
+
+/*
+void changeFreq(void){
+
+Interrupt function for changing frequency with pushbutton 
+To be implemeted 
+
+}
+*/
+
+void loop (){
+/* Make the loop as fast as possible */
+while(1){
+    phase_acumulator += tuning_word;
+    PORTB=samples[phase_acumulator >> 24]; /* Use last eight bits as lookup table index*/
+}
+}
+
