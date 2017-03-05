35300d6be781ed5f8ce38799f5435a3a055a83b3
Ovidiu Panait <ovidiuspn@gmail.com>
2017-03-05 15:37:04 +0200

    Add triangle waveform

--- waveform_generator.ino
+++ waveform_generator.ino
@@ -4,7 +4,7 @@
  */
 
 #define LT_LEN ((uint16_t)256) //Lookup table length
-#define DEFAULT_FREQ 30000.0 // default waveform frequency
+#define DEFAULT_FREQ 100.0 // default waveform frequency
 #define F_CLK  331122.51496207058823529411764705 //aproximate value for the frequency the loop runs at
 
 #define DAC_BITS 6
@@ -12,33 +12,42 @@
 
 #define SINE 0
 #define RAMP 1
+#define TRIANGLE 2
 
 uint8_t samples[LT_LEN] ={0}; /* Lookup table */
 uint8_t max_dac_amplitude = ((uint8_t)1 << DAC_BITS) - 1; 
 uint32_t prec_bits = ((uint32_t)1 << ACC_LEN);
 uint32_t phase_acc_max = ((uint32_t)1 << ACC_LEN);
+float frequency = DEFAULT_FREQ;
 
 /* We will use them in ISR */
-volatile float frequency = DEFAULT_FREQ;
-volatile uint8_t current_waveform = SINE; 
+volatile uint8_t current_waveform = TRIANGLE; 
 volatile uint32_t phase_acumulator = 0;
 volatile uint32_t tuning_word = ((frequency * phase_acc_max) / F_CLK) * prec_bits;
 
 void generate_waveform(){
-
+uint32_t i;
   switch(current_waveform){
     case SINE:
-      for (int i = 0; i < LT_LEN; ++i) {
+      for (i = 0; i < LT_LEN; ++i) {
       samples[i] = (max_dac_amplitude * (1 + sin((float)i * 2 * PI / LT_LEN))/2);
       }
     break;
     case RAMP:
-      for (int i = 0; i < LT_LEN; ++i) {
+      for (i = 0; i < LT_LEN; ++i) {
         samples[i] = i*((float)max_dac_amplitude / LT_LEN);
       }
       break;
-  }
-  
+    case TRIANGLE:
+      float dist = ((float)max_dac_amplitude / (LT_LEN/2));
+      for(i=0;i<LT_LEN;++i){
+        if(i<LT_LEN/2)
+          samples[i] = (i+1)*dist;
+        else 
+          samples[i] = (LT_LEN - i - 1)*dist;
+      }
+      break;
+  } 
 }
 
 void setup() {
